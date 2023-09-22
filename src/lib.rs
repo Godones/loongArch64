71077845e90964d51a80282e4aea8a8e2a22783f
Godones <1925466036@qq.com>
2023-09-22 23:19:15 +0800

    feat: Add several instruction abstractions

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 #![no_std]
 #![feature(asm_const)]
 #![allow(unused)]
+pub mod asm;
 mod consts;
 pub mod cpu;
 pub mod iocsr;
 pub mod ipi;
 pub mod register;
 pub mod time;
+
 const VALEN: usize = 48;
 const PALEN: usize = 48;
